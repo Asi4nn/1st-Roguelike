1adffb12e6be1b7d046a8fa127541d30bc4da672
Leo-Wang-Toronto <leo.wang142001@gmail.com>
2020-12-28 22:49:02 -0500

    added element selection for basic attacks and GUI indicator

---
+++ Assets/Scripts/ElementProjectile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementProjectile : MonoBehaviour
+{
+    public float damage;
+
+    public enum ElementType
+    {
+        Air,
+        Earth,
+        Water,
+        Fire
+    }
+    public ElementType element;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, 1000f) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name != "Player")
+        {
+            if (collision.GetComponent<Enemy>() != null)
+            {
+                collision.GetComponent<Enemy>().DealDamage(damage);
+            }
+            if (!collision.CompareTag("EnemyProjectile"))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
