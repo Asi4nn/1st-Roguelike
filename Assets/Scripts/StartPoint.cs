29d9a140525c5518038d1f4bae79b614bf30a6a5
kore4n <56659385+kore4n@users.noreply.github.com>
2020-12-28 19:03:12 -0500

    Added more sprites, worked on the tutorial, level 1, and level 2

---
+++ Assets/Scripts/StartPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // find the player
+        PlayerController thePlayer = FindObjectOfType<PlayerController>();
+        // move the player to this position
+        thePlayer.transform.position = transform.position;
+    }
+}
