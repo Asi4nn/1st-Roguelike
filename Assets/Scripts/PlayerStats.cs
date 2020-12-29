0420d359b2fa0f467b8faeac06f6e79b5f995bb9
Leo-Wang-Toronto <leo.wang142001@gmail.com>
2020-12-29 10:22:15 -0500

    added invincibility frames

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -14,6 +14,10 @@ public class PlayerStats : MonoBehaviour
     public float health;
     public float maxHealth;
 
+    private bool isInvincible = false;
+    [SerializeField] float invincibilityTime;
+    [SerializeField] float invincibilityDeltaTime;
+
     private void Awake()
     {
         if (playerStats != null)
@@ -43,9 +47,16 @@ public class PlayerStats : MonoBehaviour
 
     public void DealDamage(float damage)
     {
-        health -= damage;
-        UpdateHealth();
-        CheckDeath();
+        if (!isInvincible)
+        {
+            health -= damage;
+            UpdateHealth();
+            CheckDeath();
+            if (player != null)
+            {
+                StartCoroutine(BecomeTempInvincible());
+            }
+        }
     }
 
     public void HealCharacter(float heal)
@@ -88,7 +99,31 @@ public class PlayerStats : MonoBehaviour
         {
             healthText.text = healthText.text = Mathf.Ceil(health).ToString() + "/" + maxHealth.ToString();
         }
-        
+    }
 
+    IEnumerator BecomeTempInvincible()
+    {
+        isInvincible = true;
+        for (float i = 0; i < invincibilityTime; i += invincibilityDeltaTime)
+        {
+            // Alternate between 0 and 1 scale to simulate flashing
+            if (player.transform.localScale == Vector3.one)
+            {
+                ScalePlayer(Vector3.zero);
+            }
+            else
+            {
+                ScalePlayer(Vector3.one);
+            }
+            yield return new WaitForSeconds(invincibilityDeltaTime);
+        }
+
+        ScalePlayer(Vector3.one);
+        isInvincible = false;
+    }
+
+    private void ScalePlayer(Vector3 scale)
+    {
+        player.transform.localScale = scale;
     }
 }
