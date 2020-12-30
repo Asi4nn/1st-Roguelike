1ae71bb8c1d0dac0f959cd66a6e292ff5b20ef99
kore4n <56659385+kore4n@users.noreply.github.com>
2020-12-29 23:03:13 -0500

    Added exit point in tutorial

--- Assets/Scripts/MoveFloor.cs
+++ Assets/Scripts/MoveFloor.cs
@@ -8,9 +8,11 @@ public class MoveFloor : MonoBehaviour
     public int floorLevelToLoad;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log(collision.name);
         // check when the object collides with the player
-        if (collision.GetComponent<PlayerController>())
+        if (collision.transform.GetComponent<PlayerController>())
         {
+            Debug.Log("Loading next level");
             // load the next level
             SceneManager.LoadScene(floorLevelToLoad);
         }
